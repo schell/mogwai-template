370cab0676ea2155b497f199228a5987ede49da7
Schell Scivally <efsubenovex@gmail.com>
2020-07-23 15:51:35 -0700

    GITHUB_REF

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,12 @@
 ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
-BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+if [ -z "$GITHUB_REF" ]; then
+    GITHUB_REF="$(git rev-parse --abbrev-ref HEAD)"
+fi
+
+BRANCH=$(basename $GITHUB_REF)
+
 echo "Testing project generation from the $BRANCH branch..."
 cargo generate --git git@github.com:schell/mogwai-template.git --name gen-test --branch $BRANCH
 cd gen-test
