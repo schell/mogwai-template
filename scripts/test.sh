def23bc4c29f8ffef281331b7495448b498b2e75
Schell Scivally <efsubenovex@gmail.com>
2020-07-23 15:21:47 -0700

    target web

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,7 @@ ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+echo "Testing project generation from the $BRANCH branch..."
 cargo generate --git git@github.com:schell/mogwai-template.git --name gen-test --branch $BRANCH
 cd gen-test
 cargo build
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-extern crate log;
-extern crate console_log;
 extern crate console_error_panic_hook;
+extern crate console_log;
+extern crate log;
 extern crate mogwai;
 extern crate serde;
 extern crate serde_json;
@@ -10,21 +10,16 @@ use mogwai::prelude::*;
 use std::panic;
 use wasm_bindgen::prelude::*;
 
-
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-#[wasm_bindgen]
+#[wasm_bindgen(start)]
 pub fn main() -> Result<(), JsValue> {
-  panic::set_hook(Box::new(console_error_panic_hook::hook));
-  console_log::init_with_level(Level::Trace)
-    .unwrap();
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
+    console_log::init_with_level(Level::Trace).unwrap();
 
-  h1()
-    .text("Hello from mogwai")
-    .run()
+    h1().text("Hello from mogwai").run()
 }
