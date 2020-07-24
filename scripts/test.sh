eba58e28bc4eecafc30924a88586209c35b07f8c
Schell Scivally <efsubenovex@gmail.com>
2020-07-24 09:08:07 -0700

    enable cargo gen on cicd

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,13 +3,13 @@
 ROOT="$(git rev-parse --show-toplevel)"
 . $ROOT/scripts/common.sh
 
-if [ -z "$GITHUB_REF" ]; then
+if [ -z ${GITHUB_REF+blah} ]; then
     GITHUB_REF="$(git rev-parse --abbrev-ref HEAD)"
 fi
 
 BRANCH=$(basename $GITHUB_REF)
 
 echo "Testing project generation from the $BRANCH branch..."
-#cargo generate --git git@github.com:schell/mogwai-template.git --name gen-test --branch $BRANCH
-#cd gen-test
-#cargo build
+cargo generate --git git@github.com:schell/mogwai-template.git --name gen-test --branch $BRANCH
+cd gen-test
+cargo build
