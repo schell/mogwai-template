7c7e48afa83846316609ce6d78446e839be8da09
Schell Carl Scivally <efsubenovex@gmail.com>
2021-11-16 19:57:14 +1300

    bumped to mogwai 0.5

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,8 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-struct App {
-    clicks: u32,
-}
-
 #[derive(Clone)]
-enum AppModel {
+enum AppLogic {
     Click,
 }
 
@@ -23,46 +19,43 @@ enum AppView {
     Clicked(u32),
 }
 
-impl Component for App {
-    type DomNode = HtmlElement;
-    type ModelMsg = AppModel;
-    type ViewMsg = AppView;
-
-    fn update(&mut self, msg: &AppModel, tx: &Transmitter<AppView>, _sub: &Subscriber<AppModel>) {
+async fn logic(mut rx_logic: broadcast::Receiver<AppLogic>, tx_view: broadcast::Sender<AppView>) {
+    let mut clicks = 0u32;
+    while let Some(msg) = rx_logic.next().await {
         match msg {
-            AppModel::Click => {
-                self.clicks += 1;
-                tx.send(&AppView::Clicked(self.clicks));
+            AppLogic::Click => {
+                clicks += 1;
+                tx_view.broadcast(AppView::Clicked(clicks)).await.unwrap();
             }
         }
     }
+}
 
-    fn view(&self, tx: &Transmitter<AppModel>, rx: &Receiver<AppView>) -> ViewBuilder<HtmlElement> {
-        builder! {
-            <div
-                style=
-                    vec![
-                        "float: left;",
-                        "padding: 1em;",
-                        "border-radius:0.5em;",
-                        "border: 1px solid #ddd;",
-                        "background: #f7f7f7;",
-                        "cursor: pointer;"
-                    ].concat()
-                on:click=tx.contra_map(|_| AppModel::Click)>
-                <p>
-                    {(
-                        "Hello from mogwai!",
-                        rx.branch_map(|msg| {
-                            match msg {
-                                AppView::Clicked(1) => format!("Caught 1 click, click again 😀"),
-                                AppView::Clicked(n) => format!("Caught {} clicks", n),
-                            }
-                        })
-                    )}
-                </p>
-            </div>
-        }
+fn view(
+    tx_logic: broadcast::Sender<AppLogic>,
+    rx_view: broadcast::Receiver<AppView>,
+) -> ViewBuilder<Dom> {
+    builder! {
+        <div
+         style:float="left"
+         style:padding="1em"
+         style:border_radius=".5em"
+         style:border="1px solid #ddd"
+         style:background="#f7f7f7"
+         style:cursor="pointer"
+         on:click=tx_logic.sink().contra_map(|_| AppLogic::Click)>
+            <p>
+                {(
+                    "Hello from mogwai!",
+                    rx_view.clone().map(|msg| {
+                        match msg {
+                            AppView::Clicked(1) => format!("Caught 1 click, click again 😀"),
+                            AppView::Clicked(n) => format!("Caught {} clicks", n),
+                        }
+                    })
+                )}
+            </p>
+        </div>
     }
 }
 
@@ -71,7 +64,11 @@ pub fn main() -> Result<(), JsValue> {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     console_log::init_with_level(Level::Trace).unwrap();
 
-    let gizmo = Gizmo::from(App{ clicks: 0 });
-    let view = View::from(gizmo.view_builder());
-    view.run()
+    let (tx_logic, rx_logic) = broadcast::bounded(1);
+    let (tx_view, rx_view) = broadcast::bounded(1);
+    Component::from(view(tx_logic, rx_view))
+        .with_logic(logic(rx_logic, tx_view))
+        .build()
+        .unwrap()
+        .run()
 }
